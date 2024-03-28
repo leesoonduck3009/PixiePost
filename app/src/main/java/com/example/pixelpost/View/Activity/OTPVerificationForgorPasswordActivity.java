4c526217aa5d41283ff693a903237a1755900eac
DungLe2983 <lequocdung2983@gmail.com>
2024-03-29 00:02:30 +0700

    create verificationScreen

--- app/src/main/java/com/example/pixelpost/View/Activity/OTPVerificationForgorPasswordActivity.java
+++ app/src/main/java/com/example/pixelpost/View/Activity/OTPVerificationForgorPasswordActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,29 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.pixelpost.R;
 
-public class SignUp02Activity extends AppCompatActivity {
-    ImageView btnBack;
-    Button btnContinue;
+public class OTPVerificationForgorPasswordActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up02);
-        btnBack = findViewById(R.id.btnBack);
-        btnContinue = findViewById(R.id.btnContinue);
-
-        btnContinue.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_otpverification_forgor_password);
+        Button btnVerifyForgotPass = findViewById(R.id.btnVerifyForgotPass);
+        btnVerifyForgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUp02Activity.this, OTPVerificationSignUpActivity.class);
+                Intent intent = new Intent(OTPVerificationForgorPasswordActivity.this, ResetPasswordActivity.class);
                 startActivity(intent);
             }
         });
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+
     }
 }
--- app/src/main/java/com/example/pixelpost/View/Activity/OTPVerificationSignUpActivity.java
+++ app/src/main/java/com/example/pixelpost/View/Activity/OTPVerificationSignUpActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,28 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.pixelpost.R;
 
-public class SignUp02Activity extends AppCompatActivity {
-    ImageView btnBack;
-    Button btnContinue;
+public class OTPVerificationSignUpActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up02);
-        btnBack = findViewById(R.id.btnBack);
-        btnContinue = findViewById(R.id.btnContinue);
-
-        btnContinue.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_otpverification_sign_up);
+        Button btnVerify = findViewById(R.id.btnVerify);
+        btnVerify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUp02Activity.this, OTPVerificationSignUpActivity.class);
+                Intent intent = new Intent(OTPVerificationSignUpActivity.this, SetUpProfileActivity.class);
                 startActivity(intent);
-            }
-        });
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
             }
         });
     }
