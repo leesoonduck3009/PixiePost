a8cbdc08e98e83029694d5d39a78af3fff512f77
DungLe2983 <lequocdung2983@gmail.com>
2024-04-11 15:14:28 +0700

    toggle Password Visibility

--- app/src/main/java/com/example/pixelpost/View/Activity/Login/Login02Activity.java
+++ app/src/main/java/com/example/pixelpost/View/Activity/Login/Login02Activity.java
@@ -2,6 +2,9 @@ package com.example.pixelpost.View.Activity.Login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +30,7 @@ public class Login02Activity extends AppCompatActivity implements ILogin2Activit
     private Button btnForgotPassWord;
     private Button btnContinue;
     private EditText passwordEditText;
+    private boolean isPasswordVisible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,7 @@ public class Login02Activity extends AppCompatActivity implements ILogin2Activit
         btnForgotPassWord = findViewById(R.id.btnForgotPassWord);
         btnContinue = findViewById(R.id.btnContinue);
         passwordEditText = findViewById(R.id.passwordEditText);
+        passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
         preferenceManager = new PreferenceManager(getApplicationContext());
         presenter = new Login2ActivityPresenter(this);
         btnContinue.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +48,19 @@ public class Login02Activity extends AppCompatActivity implements ILogin2Activit
             public void onClick(View view) {
                 Intent intent = getIntent();
                 presenter.LoginByEmail(intent.getStringExtra(User.FIELD_EMAIL),passwordEditText.getText().toString());
+            }
+        });
+        passwordEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    int drawableRightWidth = passwordEditText.getCompoundDrawables()[2].getBounds().width();
+                    if (event.getRawX() >= (passwordEditText.getRight() - drawableRightWidth)) {
+                        togglePasswordVisibility();
+                        return true;
+                    }
+                }
+                return false;
             }
         });
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +77,21 @@ public class Login02Activity extends AppCompatActivity implements ILogin2Activit
             }
         });
     }
+
+    private void togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            // Ẩn mật khẩu
+            passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_off, 0);
+        } else {
+            // Hiển thị mật khẩu
+            passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_fill, 0);
+        }
+        passwordEditText.setSelection(passwordEditText.getText().length());
+        isPasswordVisible = !isPasswordVisible;
+    }
+
     @Override
     public void LoginSuccess(User user) {
         preferenceManager.putSerializable(User.FIREBASE_COLLECTION_NAME,user);
--- app/src/main/java/com/example/pixelpost/View/Activity/Signup/SignUp02Activity.java
+++ app/src/main/java/com/example/pixelpost/View/Activity/Signup/SignUp02Activity.java
@@ -2,6 +2,9 @@ package com.example.pixelpost.View.Activity.Signup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +20,8 @@ import com.example.pixelpost.R;
 public class SignUp02Activity extends AppCompatActivity {
     ImageView btnBack;
     Button btnContinue;
+    private boolean isPasswordVisible = false;
+
     EditText passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,7 @@ public class SignUp02Activity extends AppCompatActivity {
         btnBack = findViewById(R.id.btnBack);
         btnContinue = findViewById(R.id.btnContinue);
         passwordEditText = findViewById(R.id.passwordEditText);
-
+        passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,5 +53,31 @@ public class SignUp02Activity extends AppCompatActivity {
                 finish();
             }
         });
+        passwordEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    int drawableRightWidth = passwordEditText.getCompoundDrawables()[2].getBounds().width();
+                    if (event.getRawX() >= (passwordEditText.getRight() - drawableRightWidth)) {
+                        togglePasswordVisibility();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+    private void togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            // Ẩn mật khẩu
+            passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_off, 0);
+        } else {
+            // Hiển thị mật khẩu
+            passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_fill, 0);
+        }
+        passwordEditText.setSelection(passwordEditText.getText().length());
+        isPasswordVisible = !isPasswordVisible;
     }
 }
