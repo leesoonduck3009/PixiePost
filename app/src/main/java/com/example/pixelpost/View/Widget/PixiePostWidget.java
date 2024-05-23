307d956c4c0678322892eb4e711daeeb1e37d819
DungLe2983 <lequocdung2983@gmail.com>
2024-05-24 00:17:10 +0700

    fix: update onClick for Widget to MainActivity

--- app/src/main/java/com/example/pixelpost/View/Widget/PixiePostWidget.java
+++ app/src/main/java/com/example/pixelpost/View/Widget/PixiePostWidget.java
@@ -1,11 +1,14 @@
 package com.example.pixelpost.View.Widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.example.pixelpost.R;
+import com.example.pixelpost.View.Activity.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -18,7 +21,7 @@ public class PixiePostWidget extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.pixie_post_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.btnOpenWidget, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -26,9 +29,20 @@ public class PixiePostWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        for (int i=0; i < appWidgetIds.length; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+            );
+
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.pixie_post_widget);
+            views.setOnClickPendingIntent(R.id.btnOpenWidget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app
+            // widget.
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
