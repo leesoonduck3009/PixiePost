4eb83e9d297d9e01f139d18eb2db63cb6d39bd7e
DungLe2983 <lequocdung2983@gmail.com>
2024-06-13 03:05:40 +0700

    feat: create Widget Guildes Screens

---
+++ app/src/main/java/com/example/pixelpost/View/Activity/ChangePassword.java
@@ -0,0 +1,33 @@
+package com.example.pixelpost.View.Activity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.pixelpost.R;
+
+public class ChangePassword extends AppCompatActivity {
+    private ImageView btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_change_password);
+
+        btnBack = findViewById(R.id.btnBack);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnBack.setOnClickListener(v -> {
+            this.finish();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/pixelpost/View/Activity/WidgetGuilds.java
@@ -0,0 +1,51 @@
+package com.example.pixelpost.View.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.pixelpost.R;
+
+public class WidgetGuilds extends AppCompatActivity {
+
+    private ImageView btnBack, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_widget_guilds);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnNext = findViewById(R.id.btnNext);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnBack.setOnClickListener(v -> {
+            this.finish();
+            overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WidgetGuilds.this, WidgetGuilds2.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+            }
+        });
+
+
+    }
+
+
+}
